5e1a7b8f95b4303c0a0eb3c9b0593eef762c029a
andycumminstest <andyc+andycumminstest@cogapp.com>
2015-01-26 17:20:59 +0000

    test output trials

--- conf.js
+++ conf.js
@@ -4,13 +4,5 @@ exports.config = {
   multiCapabilities: [{
     browserName: 'chrome'
   }
-  ],
-
-  //  // Options to be passed to Jasmine-node.
-  // onPrepare: function() {
-  //   require('jasmine-reporters');
-  //   jasmine.getEnv().addReporter(
-  //     new jasmine.JUnitXmlReporter(null, true, true, 'junit')
-  //   );
-  //},
+  ]
 };
