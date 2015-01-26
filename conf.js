e7d2e5162f06434031e16a235117a44c47d0a92b
andycumminstest <andyc+andycumminstest@cogapp.com>
2015-01-26 16:32:41 +0000

    trying to output tests results

--- conf.js
+++ conf.js
@@ -6,5 +6,10 @@ exports.config = {
   }, {
     browserName: 'internet explorer'
   },
-  ]
+  ],
+  onPrepare: function() {
+    require('jasmine-reporters');
+    jasmine.getEnv().addReporter(
+      new jasmine.JUnitXmlReporter($CIRCLE_TEST_REPORTS, true, true));
+  }
 };
